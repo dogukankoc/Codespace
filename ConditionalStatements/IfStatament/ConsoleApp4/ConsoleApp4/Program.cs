7cfb5c19c0c57cb714357ce743100006bbcff297
Dogukan Koc <dk.dogukankoc@gmail.com>
2023-01-24 23:02:13 +0300

    consoleapp4 created

---
+++ ConditionalStatements/IfStatament/ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,27 @@
+﻿//Kullanıcının girdiği sayının pozitif, negatif yada sıfır olduğunu gösteren program
+using System;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bir sayı giriniz");
+            double number = Convert.ToDouble(Console.ReadLine());
+            
+            if (number < 0)
+            {
+                Console.WriteLine($"Girdiğiniz {number} sayısı negatiftir");
+            }
+            else if (number > 0)
+            {
+                Console.WriteLine($"Girdiğiniz {number} sayısı pozitiftir");
+            }
+            else
+            {
+                Console.WriteLine("Girdiğiniz sayı sıfırdır");
+            }
+        }
+    }
+}
