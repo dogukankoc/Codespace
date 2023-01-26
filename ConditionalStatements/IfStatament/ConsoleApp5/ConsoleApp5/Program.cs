4170d1e50efec198609b4bc314ca0c8b9b2917e2
Dogukan Koc <dk.dogukankoc@gmail.com>
2023-01-26 19:36:14 +0300

    consoleapp5 created

---
+++ ConditionalStatements/IfStatament/ConsoleApp5/ConsoleApp5/Program.cs
@@ -0,0 +1,29 @@
+﻿//Girilen iki sayıyı karşılaştırıp büyük, küçük ya da eşit olma durumunu gösteren if-else örneği.
+using System;
+
+namespace ConsoleApp5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("2 tane sayı giriniz");
+            double number = Convert.ToDouble(Console.ReadLine());
+            double numberA = Convert.ToDouble(Console.ReadLine());
+
+            if (number > numberA)
+            {
+                Console.WriteLine($"{number} > {numberA}");
+            }
+            if (number < numberA)
+            {
+                Console.WriteLine($"{number} < {numberA}");
+            }
+            else
+            {
+                Console.WriteLine($"{number} = {numberA}");
+            }
+        }
+    }
+}
