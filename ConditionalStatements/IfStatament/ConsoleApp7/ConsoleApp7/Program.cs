4d60000ad88fef2ff39bca82f06e29a4e735759f
Dogukan Koc <dk.dogukankoc@gmail.com>
2023-01-28 12:36:09 +0300

    Consoleapp7 created

---
+++ ConditionalStatements/IfStatament/ConsoleApp7/ConsoleApp7/Program.cs
@@ -0,0 +1,28 @@
+﻿//Kullanıcıdan suyun sıcaklık değerinin girilmesi istenir. Girilen sıcaklığa göre suyun hangi halde bulunduğunu gösteren örnek
+using System;
+
+namespace ConsoleApp7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Suyun sıcaklık değerini giriniz");
+            double temperatureValue = Convert.ToDouble(Console.ReadLine());
+
+            if (temperatureValue <= 0)
+            {
+                Console.WriteLine("Suyun hali katıdır");
+            }
+            else if (temperatureValue >= 100)
+            {
+                Console.WriteLine("Suyun hali gazdır");
+            }
+            else
+            {
+                Console.WriteLine("Suyun hali sıvıdır");
+            }
+        }
+    }
+}
+
