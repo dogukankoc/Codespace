4bf5bf0a3c8f250676612bfc1b8d3c5f0237623f
Dogukan Koc <dk.dogukankoc@gmail.com>
2023-06-19 20:42:50 +0300

    BlockControllerandBlockControllerViewsAdded

---
+++ SiteManagementWebApp/SiteManagement/Controllers/BlockController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualBasic;
+using SiteManagement.Models.Db;
+using SiteManagement.Models.Db.Entities;
+using SiteManagement.Models.DTOs;
+using System;
+using System.Linq;
+using System.Security.Permissions;
+
+namespace SiteManagement.Controllers
+{
+    
+    public class BlockController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [Route("block/list/{siteId}")]
+        public IActionResult List(int siteId)
+        {
+            using (var context = new SiteManagementDbContext())
+            {
+                ViewBag.BlockList = context.Blocks.Where(x => x.SiteId == siteId).ToList();
+
+                ViewBag.SiteId = siteId;
+
+            }
+            return View();
+        }
+        
+        public IActionResult CreateBlock(int siteId)
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CreateBlock(CreateBlockDTO createBlockDTO)
+        {
+            using (var context = new SiteManagementDbContext())
+            {
+                var block = new Block()
+                {
+                    BlockName = createBlockDTO.Name,
+                    SiteId =  Convert.ToInt32(HttpContext.Request.Path.Value.Split('/').LastOrDefault())
+                };
+                context.Blocks.Add(block);
+                context.SaveChanges();
+                return RedirectToAction("list", new {siteId = block.SiteId});  
+            }
+        }
+    }
+}
---
+++ SiteManagementWebApp/SiteManagement/Models/DTOs/CreateBlockDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace SiteManagement.Models.DTOs
+{
+    public class CreateBlockDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string SiteId { get; set; }
+    }
+}
