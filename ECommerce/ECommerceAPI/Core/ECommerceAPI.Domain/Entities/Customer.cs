9043e45ada4d85a764af8a8d53aa4404c57c1d93
Dogukan Koc <dk.dogukankoc@gmail.com>
2023-07-15 23:27:16 +0300

    FolderStructureDoneandEntitiesCreated

---
+++ ECommerce/ECommerceAPI/Core/ECommerceAPI.Domain/Entities/Customer.cs
@@ -0,0 +1,8 @@
+﻿using ECommerceAPI.Domain.Entities.Common;
+
+namespace ECommerceAPI.Domain.Entities
+{
+    public class Customer : BaseEntity
+    {
+    }
+}
---
+++ ECommerce/ECommerceAPI/Presentation/ECommerceAPI.API/Program.cs
@@ -0,0 +1,24 @@
+﻿
+namespace ECommerceAPI.API
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddControllers();
+
+            var app = builder.Build();
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
